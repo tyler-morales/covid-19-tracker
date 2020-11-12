9dccc55d7de80469ed6050ea2ee5bf98d7cc5fc3
Tyler Morales <moratyle@gmail.com>
2020-11-11 18:13:48 -0600

    👽  Update API URL's

--- js/requests.js
+++ js/requests.js
@@ -1,25 +1,24 @@
 const pomber_api_url_id = 'https://pomber.github.io/covid19/timeseries.json'
-const covid19Summary_api_url = 'https://api.covid19api.com/summary'
-const globalStats_api_url = 'https://corona.lmao.ninja/all'
-const states_api_url = 'https://corona.lmao.ninja/states'
+const globalStats_api_url = 'https://disease.sh/v3/covid-19/all?yesterday=true'
+const states_api_url = 'https://disease.sh/v3/covid-19/states'
 
 const news_api_key = 'd69affb88e344a3da1b2a12da50f680c'
-const news_api_url = 'https://newsapi.org/v2/everything?language=en&q=COVID&from=2020-03-16&sortBy=publishedAt&apiKey=d69affb88e344a3da1b2a12da50f680c&pageSize=20&page=1';
+const news_api_url = `https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=d69affb88e344a3da1b2a12da50f680c`;
 
 //async await states fn
 async function getStates() {
     const response = await fetch(states_api_url)
     if (response.status === 200) {
         const data = await response.json()
+        console.log('🎊 Fetched corona stats for states')
         return data
     } else {
         throw new Error('🚫 Unable to fetch Country data')
     }
 }
-
 //async await countries fn
 async function getCountries(sort) {
-    const response = await fetch(`https://corona.lmao.ninja/countries?sort=${sort}`)
+    const response = await fetch(`https://disease.sh/v3/covid-19/countries`)
     if (response.status === 200) {
         const data = await response.json()
         return data
@@ -32,8 +31,9 @@ async function getCountries(sort) {
 async function getNews() {
     const response = await fetch(news_api_url)
     if (response.status === 200) {
-        // console.log('Success 🎊, cases loaded')
+        console.log('Success 🎊, NEWS loaded')
         const data = await response.json()
+        console.log(data);
         return data
     } else {
         throw new Error('🚫 Unable to fetch News API data')
@@ -44,23 +44,10 @@ async function getNews() {
 async function getGlobalStats() {
     const response = await fetch(globalStats_api_url)
     if (response.status === 200) {
-        // console.log('Success 🎊, cases loaded')
+        console.log('Success 🎊, global stats loaded')
         const data = await response.json()
         return data
     } else {
-        throw new Error('🚫 Unable to fetch ISS data')
-    }
-}
-
-
-//async await getISS fn
-async function getCovidCases() {
-    const response = await fetch(covid19Summary_api_url)
-    if (response.status === 200) {
-        // console.log('Success 🎊, cases loaded')
-        const data = await response.json()
-        return data
-    } else {
-        throw new Error('🚫 Unable to fetch ISS data')
+        throw new Error('🚫 Unable to fetch global stats data')
     }
 }
